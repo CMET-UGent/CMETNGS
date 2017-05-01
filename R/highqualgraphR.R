dff281399b571d84c90679e811f7ad307af207aa
fpkerckh <frederiekmaarten.kerckhof@ugent.be>
2017-05-01 18:13:37 +0200

    add highqualgraphr and makebargraphrawggplot2

---
+++ R/highqualgraphR.R
@@ -0,0 +1,152 @@
+#' Publication-grade export of ggplot2 object
+#'
+#' This function takes a ggplot2 object and a series of extensions as input
+#' to export it to. A high quality graph is generated complying to PLOS ONE
+#' author guidelines.
+#'
+#' @param x a ggplot2 object.
+#' @param filename the filename without an extension.
+#' @param res resolution (ppi) of the graph, defaults to 1200
+#' @param pointsize font size (pixels), defaults to 12
+#' @param extension character vector including: "pdf", "png", "tiff", "svg"
+#'  or "postscript"
+#' @param embed a logical indicating wether in the font should be embedded
+#'  in a pdf or eps document. This is done to enhance compatibility with older
+#'  viewers or when using a non-standard font (see ?embedFonts for details). It
+#'  requires ghostscript to be installed.
+#' @param graphicstype passed on to the "type" parameter in bitmap graphics
+#' devices (defaults to "cairo", but requires cairo graphics)
+#' @param colorspace="CMYK" sets the colorspace. For printing such as in papers
+#'  the default colorspace should be CMYK but for on-screen viewing srgb may
+#'  be more desirable. Can also be set to gray to enable greyscale printing.
+#' @export
+#' @examples
+#' ## Short example
+#'
+#' # Load precomputed example data
+#' #TODO: find test object
+#'
+#'
+
+
+
+
+highqualgraphR<-function(x,filename,res=1200,pointsize=12,embed=FALSE,
+                         extension = "pdf", graphicstype="cairo",
+                         colorspace="cmyk")
+{
+  #sanity check for filename
+  assert_that(is.string(filename))
+  #see ?assertthat:assert_that and ?assertthat:is.string
+  #### Check wether x is a ggplot object ####
+  if(!is.ggplot(x)){stop("your object (x) is not a ggplot object")}
+  #### Check whether or not a supported extension is given ####
+  suportedextensions<-c("pdf", "png", "tiff", "svg","postscript")
+  extensioncheck <- extension %in% suportedextensions
+  if(length(extension)==1)
+  {
+    stopifnot(sum(extensioncheck)==1)
+  }
+  else
+  {
+    stopifnot(sum(extensioncheck)==length(extension))
+  }
+  #### check whether or not extrafonts is loaded if not check if installed ####
+  if(!("package:extrafont" %in% search())){
+    if (!(require("extrafont", character.only=TRUE, quietly=TRUE))) {
+      echo("The extrafont package is not installed or could not be loaded")
+    }else{
+      require(extrafont)
+      font_import()
+      loadfonts(quiet=TRUE)
+      loadfonts(device = "postscript",quiet=TRUE)
+    }
+  }
+
+  #######################################################################
+  # If you receive Error:
+  #     unknown family 'Arial'
+  # Then run the following code:
+  #
+  # library("extrafont")
+  # font_import()
+  # loadfonts()
+  # loadfonts(device = "postscript")
+  # don't forget to embed fonts for backwards compatibility with all pdf
+  # and postscript viewers
+  # If you receive Error:
+  # Error in embedFonts(...) GhostScript was not found
+  # on windows run: Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.06/bin/gswin64.exe")
+  # This requires to have ghostscript installed on your windows machine
+  #######################################################################
+
+  # Create subfolder figure to save your files
+  working_directory <- getwd()
+  subdirectory <- "figure/"
+  dir.create(file.path(working_directory, subdirectory), showWarnings = FALSE)
+
+  filename <- paste(subdirectory, filename, sep = "")
+
+  # define function to print your graphs
+  print_graph <- function(format){
+    if (format == "pdf")
+    {
+      pdf(file=paste(filename,".pdf",sep=""), height = 6, width = 9,
+          family = "Arial", paper = "special", onefile = FALSE,
+          colormodel=colorspace)
+    }
+    else if (format == "png")
+    {
+      png(filename=paste(filename,".png",sep=""),width=12000,height=7000,
+          units="px",res=res,pointsize=pointsize,family="Arial",
+          type=graphicstype)
+    }
+    else if (format == "tiff")
+    {
+      tiff(filename=paste(filename,".tiff",sep=""),width=12000,height=7000,
+           res=res,pointsize=pointsize,family="Arial",type=graphicstype,
+           compression="lzw")
+    }
+    else if (format == "svg")
+    {
+      svg(filename=paste(filename,".svg",sep=""),width=9,height=6,
+          pointsize=12,family="Arial")
+    }
+    else if (format == "postscript")
+    {
+      postscript(file=paste(filename,".eps",sep=""), height = 6, width = 9,
+                 family = "Arial", paper = "special", onefile = FALSE,
+                 horizontal = FALSE,colormodel=colorspace)
+    }
+    else
+    {
+      stop(paste(filename," is not a valid extension"))
+    }
+    # Print your files
+    print(x)
+    dev.off()
+    if(embed==TRUE){
+      if (format == "pdf"){
+        embed_fonts(file = paste(filename,".pdf",sep="") ,
+                    outfile = paste(filename,"_embed.pdf",sep=""))
+      }else if(format=="postscript")
+      {
+        embed_fonts(file = paste(filename,".eps",sep=""),
+                    outfile = paste(filename,"_embed.eps",sep=""))
+      }
+    }#default is FALSE. Ghostscript is generally not installed on windows systems.
+  }
+
+  # Check whether to print one or multiple plots
+  if (is.vector(extension))
+  {
+    for (format in extension)
+    {
+      print_graph(format)
+    }
+  }
+  else
+  {
+    print_graph(extension)
+  }
+}
---
+++ R/makebargraphrawggplot2.R
@@ -0,0 +1,251 @@
+#' ggplot2-based cummulative stacked bargraphs for plotting taxonomy
+#'
+#' This function uses a taxonomy file (i.e. an RDP naive bayesian fixed-rank
+#' asignment detail) and a shared file to create 100% summed stacked bargraphs.
+#' Only the top-n OTUs (in relative abundance) among all samples are displayed.
+#' All other OTUs are summed into a part of the graph designated as "other".
+#' The resulting object contaisn information about the composition of "other".
+#'
+#' @param tax is a taxonomy file, raw.
+#' @param shared is a shared (OTU-by-sample) file, filtered per sample group.
+#' @param topn is the top number of taxa selected to display (maximum 11)
+#' @param shared.abs is a logical showing wether the shared file already
+#'  consists out of absolute counts, or is really the raw csv from Ramiro
+#' @param taxlevel is either one of  "Regnum", "Phylum", "Classis",
+#'  "Ordo",  "Familia", "Genus"
+#' @param tax.prob shows whether the taxonomy file contains probabilites
+#' @param tresh is a threshold for the probability taxonomy duplicate matching
+#'  (to be implemented)
+#' @param samples is a string for the title of the generated bargraph
+#'  (e.g. "MOB","OLAND","Fecal samples")
+#' @param plot states wether or not to print the plot within the function
+#' @param ... optional arguments passed on to ggplot call
+#' @keywords taxonomic diversity
+#' @export reslist a list containing a ggplot2 object (p), the underlying
+#' data frame (ggplotdf) and the ranked taxa comprised within "others" (
+#' othertax)
+#' @examples
+#' ## Short example
+#'
+#' # Load precomputed example data
+#' #TODO: find testset
+#'
+#'
+
+makebargraphrawggplot2<-function(tax,shared,topn=8,
+                                 taxlevel=c("Regnum","Phylum", "Classis", "Ordo",
+                                            "Familia", "Genus","Species"),
+                                 shared.abs=T,tax.prob=F,tresh=85,samples,
+                                 plot=TRUE,...)
+{
+  stopifnot(topn<=11)
+  if(topn<=8){brewset<-"Set1"}else{brewset<-"Set3"}
+  ####match selected taxlevel####
+  taxlevel<-match.arg(taxlevel)
+
+  ####make sure shared file hase absolute counts to start with####
+  if(shared.abs==T)
+  {
+    shared=shared
+  }else{
+    totseqpersamp<-shared[nrow(shared),]
+    shared<-shared[-nrow(shared),]
+    effectivereads<-totseqpersamp*colSums(shared)/100
+    shared<-sweep(shared,MARGIN=2,as.numeric(effectivereads),'*') #from the Ramiro Era
+  }
+
+  seltax.str<-paste(as.character(bquote(tax)),taxlevel,sep="$")
+  seltax.val<-eval(parse(text=seltax.str))
+  unclass.val<-as.matrix(seltax.val) #variable with highest classifiable taxon of unclassifieds
+  #raw shared file, tax not preprocessed yet, auto-assign unclassified to closest taxlevel
+  unclass.index <- grep("unclassified",seltax.val)
+  incert.index  <- grep("Incertae",seltax.val)
+  #index of all "unclassified" and incertae sedis at the current taxonomic level
+  if(length(unclass.index)!=0)
+  {
+    for(i in 1:length(unclass.index))
+    {
+      unclass.rowidx<-unclass.index[i] #select unclassified row per row
+      class.colidx<-min(grep("unclassified",as.character(as.matrix(tax[unclass.rowidx,]))))-1 #column index of deepest assigned taxon
+      #consitency check to avoid stuff like Gp4 etc
+      untax <- as.matrix(tax)[unclass.index[i],class.colidx]
+      if(grepl("[0-9]",untax)&&!(taxlevel%in%c("Kingdom","Phylum"))){
+        while(grepl("[0-9]",untax)&&class.colidx>=1){
+          #browser()
+          class.colidx <- class.colidx-1
+          untax <- as.matrix(tax)[unclass.index[i],class.colidx]
+        }
+      }
+
+      if(class.colidx>=1) #maybe consider other threshold so that for summing e.g. phyla are allowed
+      {
+        unclass.val[unclass.index[i]]<-paste("unclassified ",as.matrix(tax)[unclass.index[i],class.colidx],sep="")
+      }else{unclass.val[unclass.index[i]]<-"unclassifiable"}
+    }
+  }
+  if(length(incert.index)!=0)
+  {
+    for(i in 1:length(incert.index))
+    {
+      incert.rowidx<-incert.index[i] #select unclassified row per row
+      class.colidx<-min(grep("Incertae",as.character(as.matrix(tax[incert.rowidx,]))))-1
+      #column index of deepest assigned taxon
+      if(class.colidx>=1) #maybe consider other threshold so that for summing e.g. phyla are allowed
+      {
+        unclass.val[incert.index[i]]<-paste("unclassified ",as.matrix(tax)[incert.index[i],class.colidx],sep="")
+      }else{unclass.val[incert.index[i]]<-"unclassifiable"}
+    }
+  }
+  unclass.val<-as.factor(unclass.val)
+
+  #### Sum seqcount over all duplicate taxa ####
+  if(tax.prob==F)
+  {
+    #first assign different unclassified to all unclassified OTU's at selected level
+    seltax.val<-as.matrix(seltax.val)
+    seltax.val[grep("unclassified",seltax.val)]<-paste(rep("unclassified",length(grep("unclassified",seltax.val))),seq(1,length(grep("unclassified",seltax.val))),sep="")
+    seltax.val[grep("Incertae",seltax.val)]<-paste(rep("Incertae",length(grep("Incertae",seltax.val))),seq(1,length(grep("Incertae",seltax.val))),sep="")
+    seltax.val<-as.factor(seltax.val)
+    #df that matches unclass with deepest possible classification
+    compclass.val<-data.frame(current=seltax.val,classified=unclass.val)
+    #then find duplicate values
+    multtax<-levels(as.factor(as.character(seltax.val[duplicated(seltax.val)])))
+    #then take the sum of the duplicate values
+    shared.tax<-data.frame(shared,seltax.val)
+    shared.mfsum<-matrix(0,nrow=length(multtax),ncol=ncol(shared))
+    colnames(shared.mfsum)<-names(shared)
+    #colsummed matrix for duplicate entries
+    for(i in 1:length(multtax))
+    {
+      shared.mfsum[i,]<-colSums(shared.tax[which(shared.tax$seltax.val==multtax[i]),1:ncol(shared.tax)-1])
+    }
+    shared.mfsum.tax<-data.frame(shared.mfsum,multtax) #contains all summed sequences of all duplicated or more assigned taxa
+    shared.tax.single<-shared.tax[shared.tax$seltax.val%in%setdiff(shared.tax$seltax.val,shared.mfsum.tax$multtax),] #select all single occurences
+    names(shared.mfsum.tax)<-names(shared.tax.single) #make sure that names are the same for rbind command
+    shared.tax.final<-rbind(shared.mfsum.tax,shared.tax.single)
+    rownames(shared.tax.final)<-shared.tax.final$seltax.val
+    shared.tax.final<-subset(shared.tax.final,select=-seltax.val)
+  }else
+  {
+    #first assign different unclassified to all unclassified OTU's at selected level
+    seltax.val<-as.matrix(seltax.val)
+    seltax.val[grep("unclassified",seltax.val)]<-paste(rep("unclassified",length(grep("unclassified",seltax.val))),seq(1,length(grep("unclassified",seltax.val))),sep="")
+    seltax.val<-as.factor(seltax.val)
+    #df that matches unclass with deepest possible classification
+    compclass.val<-data.frame(current=seltax.val,classified=unclass.val)
+    #then find duplicate values -- here's the difficulty when using probabilities
+    # the next command clearly illustrates the issue: levels(as.factor(as.character(seltax.val[duplicated(seltax.val)])))
+    # use the data without the probabilities for the summing sub("\\(.*\\)","",seltax.val)
+    # new proposal: everything above a certain bootstrap value gets summed (10000 Wang iterations)
+
+    # make df to do thresholding upon
+    selframe<-data.frame(do.call(rbind,strsplit(sub("\\(","\\.\\(",as.character(levels(seltax.val))),"\\.")))
+
+    selframe<-cbind(selframe,as.character(levels(unclass.val))) #as.character(levels(seltax.val))
+    names(selframe)<-c("taxon","bootstrap","full")
+
+    selframe$bootstrap<-as.numeric(sub("\\((.*)\\)","\\1",selframe$bootstrap))
+    selframe.duplo<-selframe[duplicated(selframe$taxon)|duplicated(selframe$taxon,fromLast=T),] #zeer elegante oplossing!
+    selframe.duplo.select<-selframe.duplo[selframe.duplo$bootstrap>tresh,]
+    treshedmulttax<-levels(as.factor(as.character(selframe.duplo.select[duplicated(selframe.duplo.select$taxon),]$taxon)))
+    shared.treshed.sum<-matrix(0,nrow=length(treshedmulttax),ncol=ncol(shared))
+    colnames(shared.treshed.sum)<-names(shared)
+    rownames(shared.treshed.sum)<-treshedmulttax
+    shared.tax<-data.frame(shared,seltax.val)
+    for(i in 1:length(treshedmulttax))
+    {
+      corresp<-dupframe.select[dupframe.select$taxon==treshedmulttax[i],"full"]
+      #print(corresp)
+      #print(which(as.character(shared.tax$seltax.val)==as.character(corresp)))
+      for(j in 1:length(corresp))
+      {
+        shared.treshed.sum[i,]<-shared.treshed.sum[i,]+colSums(shared.tax[which(as.character(shared.tax$seltax.val)==as.character(corresp[j])),1:ncol(shared.tax)-1])
+      }
+    }
+    shared.treshed.sum<-data.frame(shared.treshed.sum,rownames(shared.treshed.sum))
+    shared.tax.single<-shared.tax[shared.tax$seltax.val%in%setdiff(shared.tax$seltax.val,levels(selframe.duplo.select$full)),] #select all single occurences
+    names(shared.treshed.sum)<-names(shared.tax.single) #make sure that names are the same for rbind command
+    shared.tax.final<-rbind(shared.treshed.sum,shared.tax.single)
+    rownames(shared.tax.final)<-shared.tax.final$seltax.val
+    shared.tax.final<-subset(shared.tax.final,select=-seltax.val)
+
+  }
+
+
+  ####Order shared file####
+  OTU.count.tax<-rowSums(shared.tax.final) #total count of each assigned OTU
+  data_count_tax<-data.frame(shared.tax.final,OTU.count.tax)
+  data_tax <-data_count_tax[order(-data_count_tax$OTU.count.tax),] #sort descending
+  data_matrix_tax_final<-data.matrix(data_tax)
+
+  #select top  n OTUs, and remove OTU.count column
+  dimord<-dim(data_matrix_tax_final)
+  data_matrix_tax_top <- data_matrix_tax_final[1:topn,1:(dimord[2]-1)]
+  data_matrix_tax_other <- data_matrix_tax_final[topn:dimord[1],1:(dimord[2]-1)]
+  othertax <- data_matrix_tax_other
+
+  if(!is.null(dim(othertax)))
+  {
+    rownames(othertax)<- plyr::mapvalues(rownames(data_matrix_tax_other),
+                                         from=as.character(compclass.val$current),
+                                         to=as.character(compclass.val$classified),warn_missing = FALSE)
+    taxum <- rowSums(othertax)
+    #df containing all other taxa, ranked
+    suppressWarnings(othertax <- data.frame(othertax,classif=rownames(othertax),taxsum=taxum))
+    othertax <- othertax %>% dplyr::group_by(classif) %>% dplyr::summarise_each(funs(sum)) %>%
+      dplyr::arrange(desc(taxsum))
+  }else{
+    data_matrix_tax_other <- t(as.data.frame(data_matrix_tax_other))
+    rownames(data_matrix_tax_other) <- as.character(compclass.val$classified[which(compclass.val$current==rownames(data_matrix_tax_final)[nrow(data_matrix_tax_final)])])
+
+  }
+
+  #format data for plotting
+  data_matrix_tax_plot<-rbind(data_matrix_tax_top,colSums(data_matrix_tax_other)) #take a sum over all the 'other' sequences
+  data_matrix_tax_stand_plot<-decostand(data_matrix_tax_plot,method="total",MARGIN=2) #relatieve counts per staal
+  #normally, at this point, we would assign names to the "unclassifieds"
+  if(tax.prob==F)
+  {
+    unclass.plot.idx<-grep("unclassified",rownames(data_matrix_tax_stand_plot))
+    #indices of not assigned taxa for the plot
+    incert.plot.idx <- grep("Incertae",rownames(data_matrix_tax_stand_plot))
+    if(length(unclass.plot.idx)!=0){
+      for(i in 1:length(unclass.plot.idx))
+      {
+        rownames(data_matrix_tax_stand_plot)[unclass.plot.idx[i]]<-as.character(compclass.val[compclass.val$current==rownames(data_matrix_tax_stand_plot)[unclass.plot.idx[i]],]$classified)
+      }}
+    if(length(incert.plot.idx)!=0){
+      for(i in 1:length(incert.plot.idx))
+      {
+        rownames(data_matrix_tax_stand_plot)[incert.plot.idx[i]]<-as.character(compclass.val[compclass.val$current==rownames(data_matrix_tax_stand_plot)[incert.plot.idx[i]],]$classified)
+      }}
+    #check for replicated unclassifieds (!) ==> see if summing is not more "sensible" after all?
+    if(length(unique(rownames(data_matrix_tax_stand_plot)[duplicated(rownames(data_matrix_tax_stand_plot))]))!=0){
+      dupnames.sto <- unique(rownames(data_matrix_tax_stand_plot)[duplicated(rownames(data_matrix_tax_stand_plot))])
+      for(i in dupnames.sto)
+      {
+        whichdupnames <- grep(i,
+                              rownames(data_matrix_tax_stand_plot))
+        rownames(data_matrix_tax_stand_plot)[whichdupnames]<-paste0(i,1:length(whichdupnames))
+      }
+
+    }
+  }
+
+  rownames(data_matrix_tax_stand_plot)[nrow(data_matrix_tax_stand_plot)]<-"other"
+
+  ####actual plotting####
+  datamatmelt<-melt(data_matrix_tax_stand_plot)
+
+  datamatmelt$Var2o<-factor(datamatmelt$Var2,unique(as.character(datamatmelt$Var2))) #keep original order
+  p<-ggplot(data=datamatmelt,aes(x=factor(Var2o),y=value), ...)+
+    geom_bar(stat="identity",aes(fill=factor(Var1)))+
+    theme_minimal(base_size=10)+scale_fill_brewer(palette=brewset)+
+    theme(axis.text.x=element_text(angle=90,hjust=1))+
+    labs(x=NULL,y="Relative abundance",fill=paste("Deepest taxon \n(",taxlevel," level)",sep=""),
+         title=paste("Taxonomic distribution ",samples," samples",sep=""))
+  if(plot==TRUE){print(p)}
+
+  reslist <- list(ggplotdf=datamatmelt,othertax=othertax,ggplotobj=p)
+  return(reslist)
+}
