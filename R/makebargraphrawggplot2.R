b88934e81e31f1f4f240397735a179a41be9d91d
fpkerckh <frederiekmaarten.kerckhof@ugent.be>
2017-09-03 21:19:11 +0200

    code cleanup + viridis option + replace incompatible deprecated summarise_each

--- R/makebargraphrawggplot2.R
+++ R/makebargraphrawggplot2.R
@@ -8,17 +8,23 @@
 #'
 #' @param tax is a taxonomy file, raw.
 #' @param shared is a shared (OTU-by-sample) file, filtered per sample group.
-#' @param topn is the top number of taxa selected to display (maximum 11)
+#' @param topn is the top number of taxa selected to display (maximum 11,
+#' default 8)
 #' @param shared.abs is a logical showing wether the shared file already
-#'  consists out of absolute counts, or is really the raw csv from Ramiro
+#'  consists out of direct Illumina counts, or is really the raw csv from
+#'  Ramiro (default TRUE)
 #' @param taxlevel is either one of  "Regnum", "Phylum", "Classis",
 #'  "Ordo",  "Familia", "Genus"
 #' @param tax.prob shows whether the taxonomy file contains probabilites
+#' 4(default FALSE)
 #' @param tresh is a threshold for the probability taxonomy duplicate matching
-#'  (to be implemented)
-#' @param samples is a string for the title of the generated bargraph
-#'  (e.g. "MOB","OLAND","Fecal samples")
+#'  (to be implemented, default 85)
+#' @param samples is an optional string for the title of the generated bargraph
+#'  (e.g. "MOB","OLAND","Fecal samples", defualt empty)
+#' @param viridfill whether or not a discrete viridis fill pallette should be
+#' used (defaults to FALSE)
 #' @param plot states wether or not to print the plot within the function
+#' (default TRUE)
 #' @param ... optional arguments passed on to ggplot call
 #' @keywords taxonomic diversity
 #' @examples
@@ -33,7 +39,7 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
                                  taxlevel=c("Regnum","Phylum", "Classis", "Ordo",
                                             "Familia", "Genus","Species"),
                                  shared.abs=T,tax.prob=F,tresh=85,samples,
-                                 plot=TRUE,...)
+                                 viridfill=FALSE,plot=TRUE,...)
 {
   stopifnot(topn<=11)
   if(topn<=8){brewset<-"Set1"}else{brewset<-"Set3"}
@@ -48,13 +54,16 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
     totseqpersamp<-shared[nrow(shared),]
     shared<-shared[-nrow(shared),]
     effectivereads<-totseqpersamp*colSums(shared)/100
-    shared<-sweep(shared,MARGIN=2,as.numeric(effectivereads),'*') #from the Ramiro Era
+    shared<-sweep(shared,MARGIN=2,as.numeric(effectivereads),'*')
+    #from the Ramiro-Era
   }
 
   seltax.str<-paste(as.character(bquote(tax)),taxlevel,sep="$")
   seltax.val<-eval(parse(text=seltax.str))
-  unclass.val<-as.matrix(seltax.val) #variable with highest classifiable taxon of unclassifieds
-  #raw shared file, tax not preprocessed yet, auto-assign unclassified to closest taxlevel
+  unclass.val<-as.matrix(seltax.val) #variable with highest classifiable taxon
+  # of unclassifieds
+  # raw shared file, tax not preprocessed yet, auto-assign unclassified to
+  # closest taxlevel
   unclass.index <- grep("unclassified",seltax.val)
   incert.index  <- grep("Incertae",seltax.val)
   #index of all "unclassified" and incertae sedis at the current taxonomic level
@@ -63,7 +72,9 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
     for(i in 1:length(unclass.index))
     {
       unclass.rowidx<-unclass.index[i] #select unclassified row per row
-      class.colidx<-min(grep("unclassified",as.character(as.matrix(tax[unclass.rowidx,]))))-1 #column index of deepest assigned taxon
+      class.colidx<-min(grep("unclassified",
+                             as.character(as.matrix(tax[unclass.rowidx,]))))-1
+      #column index of deepest assigned taxon
       #consitency check to avoid stuff like Gp4 etc
       untax <- as.matrix(tax)[unclass.index[i],class.colidx]
       if(grepl("[0-9]",untax)&&!(taxlevel%in%c("Kingdom","Phylum"))){
@@ -73,10 +84,13 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
           untax <- as.matrix(tax)[unclass.index[i],class.colidx]
         }
       }
-
-      if(class.colidx>=1) #maybe consider other threshold so that for summing e.g. phyla are allowed
+      #maybe consider other threshold so that for summing e.g. phyla is allowed
+      if(class.colidx>=1)
       {
-        unclass.val[unclass.index[i]]<-paste("unclassified ",as.matrix(tax)[unclass.index[i],class.colidx],sep="")
+        unclass.val[unclass.index[i]]<-paste("unclassified ",
+                                             as.matrix(tax)[unclass.index[i],
+                                                            class.colidx],
+                                             sep="")
       }else{unclass.val[unclass.index[i]]<-"unclassifiable"}
     }
   }
@@ -85,11 +99,14 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
     for(i in 1:length(incert.index))
     {
       incert.rowidx<-incert.index[i] #select unclassified row per row
-      class.colidx<-min(grep("Incertae",as.character(as.matrix(tax[incert.rowidx,]))))-1
+      class.colidx<-min(grep("Incertae",
+                             as.character(as.matrix(tax[incert.rowidx,]))))-1
       #column index of deepest assigned taxon
       if(class.colidx>=1) #maybe consider other threshold so that for summing e.g. phyla are allowed
       {
-        unclass.val[incert.index[i]]<-paste("unclassified ",as.matrix(tax)[incert.index[i],class.colidx],sep="")
+        unclass.val[incert.index[i]]<-paste("unclassified ",
+                                            as.matrix(tax)[incert.index[i],
+                                                           class.colidx],sep="")
       }else{unclass.val[incert.index[i]]<-"unclassifiable"}
     }
   }
@@ -100,8 +117,14 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
   {
     #first assign different unclassified to all unclassified OTU's at selected level
     seltax.val<-as.matrix(seltax.val)
-    seltax.val[grep("unclassified",seltax.val)]<-paste(rep("unclassified",length(grep("unclassified",seltax.val))),seq(1,length(grep("unclassified",seltax.val))),sep="")
-    seltax.val[grep("Incertae",seltax.val)]<-paste(rep("Incertae",length(grep("Incertae",seltax.val))),seq(1,length(grep("Incertae",seltax.val))),sep="")
+    seltax.val[grep("unclassified",seltax.val)] <- paste(rep("unclassified",
+                                    length(grep("unclassified",seltax.val))),
+                                    seq(1,length(grep("unclassified",
+                                                      seltax.val))),sep="")
+    seltax.val[grep("Incertae",seltax.val)] <- paste(rep("Incertae",
+                                    length(grep("Incertae",seltax.val))),
+                                    seq(1,length(grep("Incertae",
+                                                      seltax.val))),sep="")
     seltax.val<-as.factor(seltax.val)
     #df that matches unclass with deepest possible classification
     compclass.val<-data.frame(current=seltax.val,classified=unclass.val)
@@ -114,38 +137,55 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
     #colsummed matrix for duplicate entries
     for(i in 1:length(multtax))
     {
-      shared.mfsum[i,]<-colSums(shared.tax[which(shared.tax$seltax.val==multtax[i]),1:ncol(shared.tax)-1])
+      shared.mfsum[i,] <- colSums(shared.tax[which(shared.tax$seltax.val==multtax[i]),
+                                             1:ncol(shared.tax)-1])
     }
-    shared.mfsum.tax<-data.frame(shared.mfsum,multtax) #contains all summed sequences of all duplicated or more assigned taxa
-    shared.tax.single<-shared.tax[shared.tax$seltax.val%in%setdiff(shared.tax$seltax.val,shared.mfsum.tax$multtax),] #select all single occurences
-    names(shared.mfsum.tax)<-names(shared.tax.single) #make sure that names are the same for rbind command
+    shared.mfsum.tax<-data.frame(shared.mfsum,multtax)
+    #contains all summed sequences of all duplicated or more assigned taxa
+    shared.tax.single <- shared.tax[shared.tax$seltax.val %in%
+                                      setdiff(shared.tax$seltax.val,
+                                              shared.mfsum.tax$multtax),]
+    #select all single occurences
+    names(shared.mfsum.tax) <- names(shared.tax.single)
+    #make sure that names are the same for rbind command
     shared.tax.final<-rbind(shared.mfsum.tax,shared.tax.single)
-    rownames(shared.tax.final)<-shared.tax.final$seltax.val
-    shared.tax.final<-subset(shared.tax.final,select=-seltax.val)
+    rownames(shared.tax.final) <- shared.tax.final$seltax.val
+    shared.tax.final <- subset(shared.tax.final,select=-seltax.val)
   }else
   {
     #first assign different unclassified to all unclassified OTU's at selected level
     seltax.val<-as.matrix(seltax.val)
-    seltax.val[grep("unclassified",seltax.val)]<-paste(rep("unclassified",length(grep("unclassified",seltax.val))),seq(1,length(grep("unclassified",seltax.val))),sep="")
+    seltax.val[grep("unclassified",seltax.val)] <- paste(rep("unclassified",
+                                  length(grep("unclassified",seltax.val))),
+                                  seq(1,length(grep("unclassified",seltax.val))),
+                                  sep="")
     seltax.val<-as.factor(seltax.val)
     #df that matches unclass with deepest possible classification
     compclass.val<-data.frame(current=seltax.val,classified=unclass.val)
     #then find duplicate values -- here's the difficulty when using probabilities
-    # the next command clearly illustrates the issue: levels(as.factor(as.character(seltax.val[duplicated(seltax.val)])))
-    # use the data without the probabilities for the summing sub("\\(.*\\)","",seltax.val)
-    # new proposal: everything above a certain bootstrap value gets summed (10000 Wang iterations)
+    # the next command clearly illustrates the issue:
+    # levels(as.factor(as.character(seltax.val[duplicated(seltax.val)])))
+    # use the data without the probabilities for the summing
+    # sub("\\(.*\\)","",seltax.val)
+    # new proposal: everything above a certain bootstrap value gets summed
+    # (10000 Wang iterations)
 
     # make df to do thresholding upon
-    selframe<-data.frame(do.call(rbind,strsplit(sub("\\(","\\.\\(",as.character(levels(seltax.val))),"\\.")))
+    selframe <- data.frame(do.call(rbind,
+                                   strsplit(sub("\\(","\\.\\(",
+                                      as.character(levels(seltax.val))),"\\.")))
+    #as.character(levels(seltax.val))
+    selframe <- cbind(selframe,as.character(levels(unclass.val)))
+    names(selframe) <- c("taxon","bootstrap","full")
 
-    selframe<-cbind(selframe,as.character(levels(unclass.val))) #as.character(levels(seltax.val))
-    names(selframe)<-c("taxon","bootstrap","full")
-
-    selframe$bootstrap<-as.numeric(sub("\\((.*)\\)","\\1",selframe$bootstrap))
-    selframe.duplo<-selframe[duplicated(selframe$taxon)|duplicated(selframe$taxon,fromLast=T),] #zeer elegante oplossing!
-    selframe.duplo.select<-selframe.duplo[selframe.duplo$bootstrap>tresh,]
-    treshedmulttax<-levels(as.factor(as.character(selframe.duplo.select[duplicated(selframe.duplo.select$taxon),]$taxon)))
-    shared.treshed.sum<-matrix(0,nrow=length(treshedmulttax),ncol=ncol(shared))
+    selframe$bootstrap <- as.numeric(sub("\\((.*)\\)","\\1",selframe$bootstrap))
+    selframe.duplo <- selframe[duplicated(selframe$taxon)|
+                                 duplicated(selframe$taxon,fromLast=T),]
+    #zeer elegante oplossing!
+    selframe.duplo.select <- selframe.duplo[selframe.duplo$bootstrap>tresh,]
+    treshedmulttax <- levels(as.factor(as.character(
+      selframe.duplo.select[duplicated(selframe.duplo.select$taxon),]$taxon)))
+    shared.treshed.sum <- matrix(0,nrow=length(treshedmulttax),ncol=ncol(shared))
     colnames(shared.treshed.sum)<-names(shared)
     rownames(shared.treshed.sum)<-treshedmulttax
     shared.tax<-data.frame(shared,seltax.val)
@@ -156,12 +196,16 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
       #print(which(as.character(shared.tax$seltax.val)==as.character(corresp)))
       for(j in 1:length(corresp))
       {
-        shared.treshed.sum[i,]<-shared.treshed.sum[i,]+colSums(shared.tax[which(as.character(shared.tax$seltax.val)==as.character(corresp[j])),1:ncol(shared.tax)-1])
+        shared.treshed.sum[i,]<-shared.treshed.sum[i,] +
+          colSums(shared.tax[which(as.character(shared.tax$seltax.val)==
+                                     as.character(corresp[j])),
+                             1:ncol(shared.tax)-1])
       }
     }
     shared.treshed.sum<-data.frame(shared.treshed.sum,rownames(shared.treshed.sum))
     shared.tax.single<-shared.tax[shared.tax$seltax.val%in%setdiff(shared.tax$seltax.val,levels(selframe.duplo.select$full)),] #select all single occurences
-    names(shared.treshed.sum)<-names(shared.tax.single) #make sure that names are the same for rbind command
+    names(shared.treshed.sum)<-names(shared.tax.single)
+    #make sure that names are the same for rbind command
     shared.tax.final<-rbind(shared.treshed.sum,shared.tax.single)
     rownames(shared.tax.final)<-shared.tax.final$seltax.val
     shared.tax.final<-subset(shared.tax.final,select=-seltax.val)
@@ -189,7 +233,8 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
     taxum <- rowSums(othertax)
     #df containing all other taxa, ranked
     suppressWarnings(othertax <- data.frame(othertax,classif=rownames(othertax),taxsum=taxum))
-    othertax <- othertax %>% dplyr::group_by(classif) %>% dplyr::summarise_each(funs(sum)) %>%
+    othertax <- othertax %>% dplyr::group_by(classif) %>%
+      dplyr::summarise_all(funs(sum)) %>%
       dplyr::arrange(desc(taxsum))
   }else{
     data_matrix_tax_other <- t(as.data.frame(data_matrix_tax_other))
@@ -237,10 +282,18 @@ makebargraphrawggplot2<-function(tax,shared,topn=8,
   datamatmelt$Var2o<-factor(datamatmelt$Var2,unique(as.character(datamatmelt$Var2))) #keep original order
   p<-ggplot(data=datamatmelt,aes(x=factor(Var2o),y=value), ...)+
     geom_bar(stat="identity",aes(fill=factor(Var1,levels=rev(levels(Var1)))))+
-    theme_minimal(base_size=10)+scale_fill_brewer(palette=brewset,direction=-1)+
-    theme(axis.text.x=element_text(angle=90,hjust=1))+
-    labs(x=NULL,y="Relative abundance",fill=paste("Deepest taxon \n(",taxlevel," level)",sep=""),
+    theme_minimal(base_size=10) + theme(axis.text.x=element_text(angle=90,
+                                                                 hjust=1))+
+    labs(x=NULL,y="Relative abundance",
+         fill=paste("Deepest taxon \n(",taxlevel," level)",sep=""),
          title=paste("Taxonomic distribution ",samples," samples",sep=""))
+
+  if(viridfill==TRUE){
+    p <- p + scale_fill_viridis(option = "D", discrete=TRUE)
+  } else {
+    p <- p + scale_fill_brewer(palette=brewset,direction = -1)
+  }
+
   if(plot==TRUE){print(p)}
 
   reslist <- list(ggplotdf=datamatmelt,othertax=othertax,ggplotobj=p)
